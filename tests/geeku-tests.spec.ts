d683014e9c77dec96aaa5f13a9d76c54cb086950
Lamitri Rich <greatgeekmusic@gmail.com>
2024-12-05 16:52:08 -0500

    Used proper casing for variables

--- tests/geeku-tests.spec.ts
+++ tests/geeku-tests.spec.ts
@@ -4,57 +4,53 @@ test.describe('to do list',function(){
         // open the browser to the web page
         // input class new-todo
         await page.goto('https://demo.playwright.dev/todomvc')
-        const todo = page.locator('input.new-todo')
-        await todo.fill('hello')
-        await todo.press('Enter')
+        const toDo = page.locator('input.new-todo')
+        await toDo.fill('hello')
+        await toDo.press('Enter')
         const listItem = page.locator('li[data-testid="todo-item"]')
         await expect(listItem).toBeVisible()
     })
     test('Click x to remove item from list', async function({ page }){
         await page.goto('https://demo.playwright.dev/todomvc')
-        const todo = page.locator('input.new-todo')
-        await todo.fill('hello')
-        await todo.press('Enter')
+        const toDo = page.locator('input.new-todo')
+        await toDo.fill('hello')
+        await toDo.press('Enter')
         const deleteButton = page.locator('button.destroy')
         const listItem = page.locator('li[data-testid="todo-item"]')
         await deleteButton.click()
-        await expect(listItem).not.toBeVisible()  
-
-        
+        await expect(listItem).not.toBeVisible()    
     })
-
     test('clicking the checkmark next to a list item reduces the items left value to 0', async function({ page }) {
         await page.goto('https://demo.playwright.dev/todomvc')
-        const todo = page.locator('input.new-todo')
-        await todo.fill('hello')
-        await todo.press('Enter')
+        const toDo = page.locator('input.new-todo')
+        await toDo.fill('hello')
+        await toDo.press('Enter')
         // complete the item by clicking the check mark
-        const firsttogtodo = page.getByTestId('todo-item').nth(0)
-        await firsttogtodo.getByRole('checkbox').check()
-        await expect(firsttogtodo).toHaveClass('completed')
+        const firstTogToDo = page.getByTestId('todo-item').nth(0)
+        await firstTogToDo.getByRole('checkbox').check()
+        await expect(firstTogToDo).toHaveClass('completed')
         // get the "X items left" element
         const itemCount = page.locator('.todo-count')
         await expect(itemCount).toContainText('0 items left')
         // validate the text value of that element
-      })
-
+    })
     test('Show items selected via filter', async function ({ page }) {
         await page.goto('https://demo.playwright.dev/todomvc')
-        const todo = page.locator('input.new-todo')
-        await todo.fill('staying out the way')
-        await todo.press('Enter')
-        await todo.fill('nahh forreal')
-        await todo.press('Enter')
-        const firsttogtodo = page.getByTestId('todo-item').nth(0)
-        await firsttogtodo.getByRole('checkbox').check()      
-        await expect(firsttogtodo).toHaveClass('completed')
+        const toDo = page.locator('input.new-todo')
+        await toDo.fill('staying out the way')
+        await toDo.press('Enter')
+        await toDo.fill('nahh forreal')
+        await toDo.press('Enter')
+        const firstTogToDo = page.getByTestId('todo-item').nth(0)
+        await firstTogToDo.getByRole('checkbox').check()      
+        await expect(firstTogToDo).toHaveClass('completed')
         await page.pause()
-        const sectogtodo = page.getByTestId('todo-item').nth(1)
-        await sectogtodo.getByRole('checkbox').check()      
-        await expect(sectogtodo).toHaveClass('completed')
+        const secTogToDo = page.getByTestId('todo-item').nth(1)
+        await secTogToDo.getByRole('checkbox').check()      
+        await expect(secTogToDo).toHaveClass('completed')
         await page.pause()
-        await expect(firsttogtodo).toHaveClass('completed');
-        await expect(sectogtodo).toHaveClass('completed');
+        await expect(firstTogToDo).toHaveClass('completed');
+        await expect(secTogToDo).toHaveClass('completed');
         const activeLink = page.getByRole('link', {name: 'Active'})
         const completedLink = page.getByRole('link', {name: 'Completed'})
         await activeLink.click()
@@ -64,7 +60,4 @@ test.describe('to do list',function(){
         await expect(completedLink).toHaveClass('selected')
         await page.pause()
     })
-
-
-    
 })
